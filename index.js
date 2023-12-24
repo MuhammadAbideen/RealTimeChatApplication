3b9db566e6c2c94aa64aec3ea90c92aeb8e78b25
MuhammadAbideen <muhammadabideen369@gmail.com>
2023-12-24 18:58:00 +0530

    new

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
         const user = newUser(socket.id, username, room);
         socket.join(user.room);
 
-        socket.broadcast.to(user.room).emit('message', formatMessage("Aitribe", `${user.username} has joined the room`));
+        socket.broadcast.to(user.room).emit('message', formatMessage("ChatApp", `${user.username} has joined the room`));
 
         io.to(user.room).emit('roomUsers', {
             room: user.room,
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
                 io.to(recipient.id).emit('message', formatMessage(`(Private from ${user.username})`, privateMessage));
                 socket.emit('message', formatMessage(`(Private to ${recipient.username})`, privateMessage));
             } else {
-                socket.emit('message', formatMessage('Aitribe', 'User not found or cannot send private message to yourself.'));
+                socket.emit('message', formatMessage('ChatApp', 'User not found or cannot send private message to yourself.'));
             }
         } else {
             io.to(user.room).emit('message', formatMessage(user.username, msg));
@@ -50,7 +50,7 @@ io.on('connection', (socket) => {
         const user = exitRoom(socket.id);
 
         if (user) {
-            io.to(user.room).emit('message', formatMessage("Aitribe", `${user.username} has left the room`));
+            io.to(user.room).emit('message', formatMessage("ChatApp", `${user.username} has left the room`));
 
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
